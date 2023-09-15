49a8f515061ac23a11dba771cd3ea8941bd9d3b5
Jakub Romaniszyn <jakub.romaniszyn@gds.ey.com>
2023-09-15 11:58:19 +0200

    Add production configurations for container and marketing + add container github action

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
-//mount function to start up the app
+// Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <App/>,
-        el
-    );
+    ReactDOM.render(<App />, el);
 };
-//if we are in dev and isolation call mount
+
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
+
     if (devRoot) {
         mount(devRoot);
     }
 }
-// we are running through container we export the mount function
-export {mount};
+
+// We are running through container
+// and we should export the mount function
+export { mount };
